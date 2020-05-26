813270120dd47c9307404322fa8cad1937c91a43
prpereiras89 <prpereiras89@gmail.com>
2020-05-26 05:34:27 -0300

    commited at:25/05/2020 19:25

--- covid_update.py
+++ covid_update.py
@@ -442,6 +442,16 @@ with open("index.html", "w") as f:
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
                     <link rel="stylesheet" href="dashboard_covid.css">
                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+                    <script>
+                        $(document).ready(function() {
+                            var viewMode = getCookie("view-mode");
+                            if(viewMode == "desktop"){
+                                viewport.setAttribute('content', 'width=1024');
+                            }else if (viewMode == "mobile"){
+                                viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+                            }
+                        });
+                    </script>
                 </head>
                 <body>
                     <div class="container-fluid">
