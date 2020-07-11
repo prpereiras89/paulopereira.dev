76428176a0a376fa8e5c00a8f24d0f5c47d5b8ee
prpereiras89 <prpereiras89@gmail.com>
2020-07-11 14:33:26 -0300

    v0.7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ab25e7241f6c34ef32e68eb3ba751c2.js"
+  "/precache-manifest.18c24ad8610dddc87752c21edddcd1a9.js"
 );
 
 self.addEventListener('message', (event) => {
