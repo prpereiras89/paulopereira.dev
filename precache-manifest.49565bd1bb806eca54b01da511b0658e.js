00c414eee9b7c8a09bcaec18c76c0c8ca659ce98
prpereiras89 <prpereiras89@gmail.com>
2021-03-18 16:33:02 -0300

    latest:18-Mar-2021

---
+++ precache-manifest.49565bd1bb806eca54b01da511b0658e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733de69d44107e5db367f80b78c5d7b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60319c09b830fe7f05d2",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "356e21e1fd91207463b4",
+    "url": "/static/css/main.308af0db.chunk.css"
+  },
+  {
+    "revision": "60319c09b830fe7f05d2",
+    "url": "/static/js/2.00828faa.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.00828faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356e21e1fd91207463b4",
+    "url": "/static/js/main.cf3753c2.chunk.js"
+  },
+  {
+    "revision": "aec2105d9ac075281e36",
+    "url": "/static/js/runtime-main.e61d08df.js"
+  },
+  {
+    "revision": "4e993032980ae418aa9f5fbcec927515",
+    "url": "/static/media/pp_rounded.4e993032.png"
+  },
+  {
+    "revision": "eeb08a55d4ac2d765f2209d2ea9dc4ab",
+    "url": "/static/media/pp_rounded_contact.eeb08a55.png"
+  }
+]);
