1511078d75cee7d83f96e70134265609e5f960c7
Paulo Pereira <paulo.pereira.phd@gmail.com>
2023-05-13 12:12:45 -0300

    update: add the felipe's paper

--- src/components/publications.js
+++ src/components/publications.js
@@ -20,6 +20,20 @@ class PublicationsPage extends Component {
             <h3 style={{ fontFamily: "Oxygen, sans-serif" }}>Journals</h3>
             {/** Journals*/}
             <Papers
+              year={2023}
+              title={
+                "Dependability Evaluation of a Smart Poultry House: Addressing Availability Issues Through the Edge, Fog, and Cloud Computing"
+              }
+              link={
+                "https://ieeexplore.ieee.org/document/10123963"
+              }
+              metaData={"IEEE Transactions on Industrial Informatics"}
+              abstract={
+                "Internet of Things (IoT) applications equip rural producers with decision support tools and automated solutions that boost agribusiness productivity, quality, and profit. However, most poultry farmers still use conventional methods of operation in which human workers carry out all routines for monitoring and controlling their farms at the expense of greater productivity. One of these human activities is manual weighing, which can be replaced by non-intrusive methods such as computational vision applications that estimate live poultry's weight using video cameras. Since Internet of Things (IoT) devices may have low computing power limiting the ability to process the data locally, they can transfer it to a fog or cloud data center, where they are processed. This paper aims to conduct a dependability study of a poultry house automated with a computer vision-based system for estimating poultry weight considering hierarchical models (e.g., Markov chain, Reliability Block Diagram, and closed-form equation) to represent the whole system and obtain steady-state availability and annual downtime. In addition, our purpose is to consider and compare different architectural solutions, such as edge and fog computing-based solutions. The proposed solution verified that a cloud-based application with no redundancy has a downtime of 34.14% and 9.176% hours when considering a hot-standby redundancy strategy in the office node of a cloud solution."
+              }
+            />
+
+            <Papers
               year={2022}
               title={
                 "A performance modeling framework for microservices-based cloud infrastructures"
